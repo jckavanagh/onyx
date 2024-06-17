06e39f51470a442dab03df83b8a6feeaf5c0c4fc
Jessica Kavanagh <kavanagh.jc@gmail.com>
2024-06-17 14:17:54 -0500

    Revoking Tokens

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import exceptions
 from .serializers import UserSerializer
-from .models import User
+from .models import User, UserToken
 from .authentication import create_access_token, create_refresh_token, decode_refresh_token, JWTAuthentication
+import datetime
 
 class RegisterAPIView(APIView):
     def post(self, request):
@@ -34,6 +35,13 @@ class LoginAPIView(APIView):
         access_token = create_access_token(user.id)
         refresh_token = create_refresh_token(user.id)
 
+        UserToken.objects.create(
+            user_id=user.id,
+            token=refresh_token,
+            expired_at=datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=30)
+        )
+
+
         response = Response()
         response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
 
@@ -54,6 +62,13 @@ class RefreshAPIView(APIView):
         refresh_token = request.COOKIES.get('refresh_token')
         id = decode_refresh_token(refresh_token)
 
+        if not UserToken.objects.filter(
+            user_id=id,
+            token=refresh_token,
+            expirted_at__gt=datetime.datime.now(tz=datetime.timezone.utc)
+        ).exists():
+            raise exceptions.AuthenticationFailed('unauthenticated')
+
         access_token = create_access_token(id)
 
         return Response({
@@ -61,7 +76,11 @@ class RefreshAPIView(APIView):
         })
     
 class LogoutAPIView(APIView):
+
     def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        UserToken.objects.filter(token=refresh_token).delete()
+
         response = Response()
         response.delete_cookie(key='refresh_token')
         response.data = {
