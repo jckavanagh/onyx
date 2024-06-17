5cb81f7ca31d722254ecf9a3492bd5e73352b309
Jessica Kavanagh <kavanagh.jc@gmail.com>
2024-06-17 11:19:52 -0500

    JWT Middleware

--- backend/core/authentication.py
+++ backend/core/authentication.py
@@ -1,11 +1,27 @@
 import jwt, datetime
 from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from core.models import User
 # from datetime import timezone
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+
+            user = User.objects.get(pk=id)
+
+            return (user, None)
+
+        raise exceptions.AuthenticationFailed('unauthenticated')
 
 def create_access_token(id):
     return jwt.encode({
         'user_id': id,
-        'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=30),
+        'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=60),
         'iat': datetime.datetime.now(datetime.timezone.utc)
     }, 'access_secret', algorithm='HS256')
 
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,11 @@
 # from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authentication import get_authorization_header
 from rest_framework import exceptions
 from .serializers import UserSerializer
 from .models import User
-from .authentication import create_access_token, decode_access_token, create_refresh_token
+from .authentication import create_access_token, create_refresh_token, JWTAuthentication
 
-# Create your views here.
 class RegisterAPIView(APIView):
     def post(self, request):
         data = request.data
@@ -46,17 +44,7 @@ class LoginAPIView(APIView):
         return response
 
 class UserAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+
     def get(self, request):
-        auth = get_authorization_header(request).split()
-
-        if auth and len(auth) == 2:
-            token = auth[1].decode('utf-8')
-            id = decode_access_token(token)
-
-            user = User.objects.get(pk=id)
-
-            if user:
-                serializer = UserSerializer(user)
-                return Response(serializer.data)
-
-        raise exceptions.AuthenticationFailed('unauthenticated')
+        return Response(UserSerializer(request.user).data)
