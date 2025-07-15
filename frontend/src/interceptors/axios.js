13ce334176062b98281e8d0ff3fef9ad955b7f4b
Jessica Kavanagh <kavanagh.jc@gmail.com>
2025-07-15 18:30:51 -0500

    axios Base URL https correction

--- frontend/src/interceptors/axios.js
+++ frontend/src/interceptors/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000/api/";
-axios.defaults.baseURL = "http://api.jckavanagh.com:8000/api/";
+axios.defaults.baseURL = "https://api.jckavanagh.com:8000/api/";
 // axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
 // axios.defaults.baseURL = "http://134.209.212.110:8000/api/";
 // axios.defaults.baseURL = "http://backend:8000/api/";
